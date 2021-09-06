97d66374ff40fdd1a61ed84edc30be1b86ba5a7f
yurika <yrpn120293@gmail.com>
2021-09-07 04:58:44 +0900

    [Add]404.html

--- js/script.js
+++ js/script.js
@@ -3,17 +3,19 @@
 const headerBtn = document.getElementById('js_drawerBtn');
 const headerNav = document.getElementById('js_drawerContents');
 const headerNavItem = document.getElementsByClassName('js_navItem');
-const drawerFixed = document.getElementById('js_drawerFixed');
+const drawerFixed = document.getElementsByClassName('js_drawerFixed');
 
 headerBtn.addEventListener('click', () => {
     headerBtn.classList.toggle('is_open');
     headerNav.classList.toggle('is_open');
-    if(headerBtn.classList.contains('is_open')) {
-        setTimeout( () => {
-            drawerFixed.classList.add('is_fixed');
-        }, 1000);
-    } else {
-        drawerFixed.classList.remove('is_fixed');
+    for(let i = 0; i < drawerFixed.length; i++) {
+        if(headerBtn.classList.contains('is_open')) {
+            setTimeout( () => {
+                drawerFixed[i].classList.add('is_fixed');
+            }, 1000);
+        } else {
+            drawerFixed[i].classList.remove('is_fixed');
+        }
     }
 });
 
