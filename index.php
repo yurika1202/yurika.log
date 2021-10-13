dc9af301321f0dce883818e11bde4f14bd9b7ac7
yurika <yrpn120293@gmail.com>
2021-10-14 05:49:40 +0900

    [Add]sidebar.php

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php get_header(); ?>
 
 <main class="ly_main bl_2colUnit__main js_drawerFixed">
-
     <!-- articleList -->
     <?php get_template_part('template-parts/article'); ?>
 
     <!-- widget -->
-    <aside class="bl_aside_wrap bl_2colUnit_rightItem__main">
-
-        <?php get_template_part('template-parts/widget/profile-widget'); ?>
-
-        <div class="bl_2colUnit__widget">
-            <div class="bl_search bl_common_blockWrap hp_mt40 hp_display__widget">
-                <?php get_template_part('template-parts/widget/search-widget'); ?>
-            </div><!-- /.bl_search -->
-
-            <div class="bl_category bl_common_blockWrap hp_mt40 hp_display__widget">
-                <?php get_template_part('template-parts/widget/category-widget'); ?>
-            </div><!-- /.bl_category -->
-        </div><!-- /.bl_widget__2col -->
-    </aside><!-- /.bl_aside_wrap -->
-
+    <?php get_sidebar(); ?>
 </main>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -274,37 +274,7 @@
         <?php endif; ?>
 
         <!-- widget -->
-        <aside class="bl_aside_wrap bl_2colUnit_rightItem__main">
-
-            <?php get_template_part('template-parts/widget/profile-widget'); ?>
-
-            <div class="bl_2colUnit__widget">
-                <div class="bl_search bl_common_blockWrap hp_mt40 hp_display__widget">
-                    <?php get_template_part('template-parts/widget/search-widget'); ?>
-                </div><!-- /.bl_search -->
-
-                <div class="bl_category bl_common_blockWrap hp_mt40 hp_display__widget">
-                    <?php get_template_part('template-parts/widget/category-widget'); ?>
-                </div><!-- /.bl_category -->
-            </div><!-- /.bl_widget__2col -->
-
-            <div class="bl_toc bl_toc__aside bl_common_blockWrap hp_mt40">
-                <p class="bl_widget_title">Mokuji</p><!-- /.bl_category_title -->
-
-                <ul class="bl_toc_list">
-                    <li class="bl_toc_item">
-                        <a href="">h2のタイトルがはいります</a>
-
-                        <ul class="bl_toc_list__child">
-                            <li class="bl_toc_item__child"><a href="">h3のタイトルがはいります</a></li>
-                            <li class="bl_toc_item__child"><a href="">h3のタイトルh3のタイトルがはいります</a></li>
-                        </ul><!-- /.bl_toc_list -->
-                    </li><!-- /.bl_toc_item -->
-                    <li class="bl_toc_item"><a href="">h2のタイトルがはいります</a></li><!-- /.bl_toc_item -->
-                    <li class="bl_toc_item"><a href="">h2のタイトルがはいります</a></li><!-- /.bl_toc_item -->
-                </ul><!-- /.bl_toc_list -->
-            </div><!-- /.bl_toc -->
-        </aside><!-- /.bl_aside_wrap -->
+        <?php get_sidebar('single'); ?>
     </div><!-- /. bl_2colUnit__main -->
 
 </main>
