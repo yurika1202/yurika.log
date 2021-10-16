d4aa3c7bcc2b077a7c498000e4387497ed6c1631
yurika <yrpn120293@gmail.com>
2021-10-17 00:03:56 +0900

    [Add]ボタンスタイル追加

--- js/editor.js
+++ js/editor.js
@@ -14,4 +14,22 @@
 		name: "blockquote_default",
 		label: "引用デフォルト",
 	});
+
+    // ボタン
+    wp.blocks.registerBlockStyle("core/buttons", {
+		name: "btn_flex_left",
+		label: "ボタン横並び左",
+	});
+    wp.blocks.registerBlockStyle("core/buttons", {
+		name: "btn_flex_center",
+		label: "ボタン横並び中央",
+	});
+    wp.blocks.registerBlockStyle("core/button", {
+		name: "btn_default",
+		label: "ボタンデフォルト",
+	});
+    wp.blocks.registerBlockStyle("core/button", {
+		name: "btn_outline",
+		label: "ボタンアウトライン",
+	});
 })();
